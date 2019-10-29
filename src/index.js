695878d9aa727b90d1d606acbf151df098f2c2aa
Matt Ng <matt.ng@reece.com.au>
2019-10-29 22:00:54 +1100

    Gameboard const

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const yargs = require("yargs");
 const storage = require('node-persist');
 
 //  Game board 5 x 5 matrix
-let gameBoard = [
+const gameBoard = [
     [1, 2, 3, 4, 5],
     [6, 7, 8, 9, 10],
     [11, 12, 13, 14, 15],
